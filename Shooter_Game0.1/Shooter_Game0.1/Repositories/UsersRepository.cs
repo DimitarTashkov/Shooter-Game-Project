03768d38a628183c1e563f291e996422d106fff8
DimitarTashkov <mitkotashkov1979@gmail.com>
2023-10-29 15:21:52 +0200

    finished architecture

---
+++ Shooter_Game0.1/Shooter_Game0.1/Repositories/UsersRepository.cs
@@ -0,0 +1,28 @@
+﻿using Shooter_Game0._1.Models.Users.Contracts;
+using Shooter_Game0._1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shooter_Game0._1.Repositories
+{
+    public class UsersRepository : IRepository<IUser>
+    {
+        private List<IUser> users;
+        public UsersRepository()
+        {
+            users = new List<IUser>();
+        }
+        public void AddNew(IUser name)
+        {
+            users.Add(name);
+        }
+
+        public IReadOnlyCollection<IUser> Models() => users.AsReadOnly();
+
+        public bool RemoveByName(string typeName) => users.Remove(users.FirstOrDefault(x => x.Username == typeName));
+
+    }
+}
---
+++ Shooter_Game0.1/Shooter_Game0.1/Utilities/Messages/OutputMessages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shooter_Game0._1.Utilities.Messages
+{
+    public static class OutputMessages
+    {
+        public static string EnemyWasAdded = "{0} has been added successfully to our enemies!";
+        public static string MapWasAdded = "{0} has been added successfully to our maps!";
+        public static string WeaponWasAdded = "{0} has been added successfully to our weapons!";
+        public static string EnemiesGenerated = "{0} enemies has been generated on {1}! ";
+        public static string EnemyWasShotFor = "{0} has been shot for {1} damage with {2}!";
+        public static string EnemySurvived = "{0} survived with {1} hp !";
+        public static string EnemyWasKilled = "{0} enemy was successfully killed with {1}!";
+    }
+}
